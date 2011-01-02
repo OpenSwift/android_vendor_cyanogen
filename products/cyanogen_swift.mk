50584954fd90ea49ea9fdc768cd406d161f9aca8
Quarx2k <agent00791@gmail.com>
2011-01-02 13:17:05 -0800

    Deleted unused options

--- products/cyanogen_swift.mk
+++ products/cyanogen_swift.mk
@@ -1,12 +1,6 @@
 # Inherit AOSP device configuration for dream_sapphire.
 $(call inherit-product, device/lg/swift/full_swift.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common.mk)
-
-# Include GSM-only stuff
-$(call inherit-product, vendor/cyanogen/products/gsm.mk)
-
 #
 # Setup device specific product configuration.
 #
@@ -17,34 +11,10 @@ PRODUCT_MODEL := GT540
 PRODUCT_MANUFACTURER := LG
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
 
-# Build kernel
-PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
-PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel
-PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=openetna
-
 # Extra DS overlay
 PRODUCT_PACKAGE_OVERLAYS += device/lg/swift/overlay
-
-# Enable Compcache by default on D/S
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/common
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.compcache.default=1
+        ro.modversion=OpenSwift/CyanogenMod-6.1.1
 
-#
-# Set ro.modversion
-#
-ifdef CYANOGEN_NIGHTLY
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=OpenSwift/CyanogenMod-6.1-$(shell date +%m%d%Y)-NIGHTLY
-else
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=OpenSwift/CyanogenMod-6.1
-endif
-
-# Use the audio profile hack
-WITH_DS_HTCACOUSTIC_HACK := true
-
-#
-# Copy DS specific prebuilt files
-#
-PRODUCT_COPY_FILES +=  \
-    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
